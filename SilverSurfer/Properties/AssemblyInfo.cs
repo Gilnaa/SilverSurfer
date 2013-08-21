a990d52092d41bc905d77fefc1e9cf17d25433b1
Gilad Naaman <gilad.doom@gmail.com>
2013-08-21 21:20:06 +0300

    Updated assembly metadata.

--- SilverSurfer/Properties/AssemblyInfo.cs
+++ SilverSurfer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SilverSurferLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SilverSurfer")]
+[assembly: AssemblyDescription("SilverSurfer is a mathematical expression parsing and evaluation library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Gilad Naaman")]
 [assembly: AssemblyProduct("SilverSurferLib")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- SilverSurferRT/Properties/AssemblyInfo.cs
+++ SilverSurferRT/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SilverSurfer")]
+[assembly: AssemblyTitle("SilverSurferRT")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Gilad Naaman")]
 [assembly: AssemblyProduct("SilverSurfer")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
